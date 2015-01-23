841d2739b6a757b0de37e38fc8d077fd1a477719
jamalping <silvercreek@Ryans-MBA.local>
2015-01-23 17:02:42 +0800

    Initial Commit

---
+++ SortArray/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SortArray
+//
+//  Created by jamalping on 15-1-23.
+//  Copyright (c) 2015年 jamalping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SortArrayTests/SortArrayTests.m
@@ -0,0 +1,40 @@
+//
+//  SortArrayTests.m
+//  SortArrayTests
+//
+//  Created by jamalping on 15-1-23.
+//  Copyright (c) 2015年 jamalping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SortArrayTests : XCTestCase
+
+@end
+
+@implementation SortArrayTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
